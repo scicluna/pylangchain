06bb36715d500cf15d6716ce5d359f4ff25af103
scicluna <88948700+scicluna@users.noreply.github.com>
2023-09-21 21:21:21 -0400

    fascinating not sure how to use

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SimpleSequentialChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -26,12 +27,20 @@ scriptTemplate = PromptTemplate(
     template='write me a youtube video script based on this title: {title}'
 )
 
-titleChain = LLMChain(llm=llm, prompt=titleTemplate, verbose=True, output_key='title')
-scriptChain = LLMChain(llm=llm, prompt=scriptTemplate, verbose=True, output_key='script')
+#memory
+memory = ConversationBufferMemory(input_key='topic', memory_key='chatHistory')
+
+#actual chaining
+titleChain = LLMChain(llm=llm, prompt=titleTemplate, verbose=True, output_key='title', memory=memory)
+scriptChain = LLMChain(llm=llm, prompt=scriptTemplate, verbose=True, output_key='script', memory=memory)
 sequentialChain = SequentialChain(chains=[titleChain, scriptChain], input_variables=['topic'], output_variables=['title', 'script'], verbose=True)
+
 
 #show stuff to screen
 if prompt:
     response = sequentialChain({'topic':prompt})
     st.write(response['title'])
     st.write(response['script'])
+
+    with st.expander('Message History'):
+        st.info(memory.buffer)
